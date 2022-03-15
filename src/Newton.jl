9b0e8b176cfd1542fdc00a58e574170816f76e8f
Kiarash Hosseini <k.hosseini1130@gmail.com>
2022-03-14 19:13:12 -0700

    The First Comments

--- src/Newton.jl
+++ src/Newton.jl
@@ -1,5 +1,52 @@
 module Newton
 
 # Write your package code here.
+using Statistics, LinearAlgebra, ForwardDiff
 
+function newtonroot(f,f′;x₀,tol=1E-7,maxiter=1000)
+    println("the first dispach")
+    x=zeros(maxiter+1)
+    x_old=x₀
+    i=1
+    norm_dif=Inf
+    while i <= maxiter &&  norm_dif>=tol 
+        x_new=x_old - f(x_old)/f′(x_old)
+        i+=1
+        norm_dif=norm(x_new-x_old)
+        x_old=x_new
+    end
+    if i == maxiter+1
+        return nothing
+    else 
+        return (iteration=i, norm_dif=norm_dif, x=x_old)
+    end
+end
+
+
+function newtonroot(f;x₀,tol = 1E-7,maxiter = 1000)
+    println("the second dispach")
+    x=zeros(maxiter+1)
+    x_old=x₀
+    i=1
+    norm_dif=Inf
+    f_prime(x)=ForwardDiff.derivative(f,x)
+    while i <= maxiter &&  norm_dif>=tol 
+        x_new=x_old - f(x_old)/f_prime(x_old)
+        i+=1
+        norm_dif=norm(x_new-x_old)
+        x_old=x_new
+    end
+    if i == maxiter+1
+        return nothing
+    else 
+        return (iteration=i, norm_dif=norm_dif, x=x_old)
+    end
+end
+
+
+f(x) = 2+ x^2
+
+newtonroot(f, x₀=1)
+
+export newtonroot
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,47 @@
 using Newton
 using Test
 
+
+f1(x)=x^3
+f1_prime(x)=3*x^2
+
+f2(x) = sin(x)
+f2_prime(x) = cos(x)
+
+f3(x) = log(x)
+f3_prime(x) = 1//x
+
+
+fs=[f1,f2,f3]
+f_primes=[f1_prime,f2_prime,f3_prime]
+f_nc(x) = 2+ x^2
+f_nc_prime(x) = 2*x
+
 @testset "Newton.jl" begin
-    # Write your tests here.
+    
+    for (f,f_prime) in zip(fs,f_primes) # Several test for the root of a known function,given f and analytical f′
+        @test newtonroot(f,x₀=1).x≈newtonroot(f,f_prime,x₀=1).x 
+    end
+
+    for (f,f_prime) in zip(fs,f_primes)
+        for i in 1:200:1000 # test to ensure that maxiter is working
+            @test  newtonroot(f,f_prime,x₀=1, maxiter=i)===nothing || newtonroot(f,f_prime,x₀=1, maxiter=i).iteration <= i
+        end
+    end
+
+    for (f,f_prime) in zip(fs,f_primes)
+        for i in 1:200:1000 # test to ensure that maxiter is working
+            @test  newtonroot(f,f_prime,x₀=1, maxiter=i)===nothing || newtonroot(f,f_prime,x₀=1, maxiter=i).iteration <= i
+        end
+    end
+
+    @test newtonroot(f_nc,x₀=1) === nothing # Test of non-convergence
+    @test newtonroot(f_nc,f_nc_prime,x₀=1)=== nothing # Test of non-convergence
+
+    @test newtonroot(f1,f1_prime,x₀=BigFloat(1)).x |> typeof == BigFloat # BigFloat roots
+    @test newtonroot(f1,x₀=BigFloat(1)).x |> typeof == BigFloat # BigFloat roots
+
+    @test newtonroot(f1,x₀=1) == nothing ? true : newtonroot(f1,x₀=1).norm_dif <=1E-5
+    @test newtonroot(f1,f1_prime, x₀=1) == nothing ? true : newtonroot(f1,x₀=1).norm_dif <=1E-5
+
 end
